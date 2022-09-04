54cd4c97d4a59a32dbe367d029cb0d1afedcda1e
s-a-b-r <po_fasty_grisha@mail.ru>
2022-09-04 15:08:04 +0400

    Initial commit. Added layout, index page, product page

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'main.index',
+      component: () => import('../views/main/Index.vue')
+    },
+    {
+      path: '/products',
+      name: 'main.products',
+      component: () => import('../views/product/Index.vue')
+    }
+  ]
+})
+
+export default router
